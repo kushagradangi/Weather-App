c64e174bafe0a11f6d7dc146f4088ed18bb32e09
kushnotfromagra <kushagradangi10@gmail.com>
2025-01-22 16:00:40 +0530

    assets added

--- index.js
+++ index.js
@@ -17,8 +17,6 @@ async function fetchWeatherDetails(){
          const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
          const data = await response.json();
 
-        console.log("Weather App:-> ", data);
-
         renderWeatherInfo(data);
     }
     catch(err){
