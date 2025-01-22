2b89048a9181757cf882811d293602cfa25ca265
kushnotfromagra <kushagradangi10@gmail.com>
2025-01-22 23:22:50 +0530

    html values fetched

--- index.js
+++ index.js
@@ -1,75 +1,9 @@
-const API_KEY = "d2d9bccc7b48e821930131fca809ce88";
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
+const userContainer = document.querySelector(".weather-containe");
+const grantAccessContainer = document.querySelector(".grant-location-container");
+const searchForm = document.querySelector("[data-searchForm]");
+const loadingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".user-info-container");
 
-//function that renders API data on Display
-function renderWeatherInfo(data){
-
-    newPara = document.createElement("p");
-    newPara.textContent = `${data?.main?.temp.toFixed(2)} °C`
-    document.body.appendChild(newPara);
-}
-
-//function that fetch API and converts data into JSON format
-async function fetchWeatherDetails(){
-
-    try{
-        let city = "goa"
-
-         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`);
-         const data = await response.json();
-
-        renderWeatherInfo(data);
-    }
-    catch(err){
-
-    }
-
-    
-
-}
-
-fetchWeatherDetails();
-
-
-//to switch pages
-function switchTab(clickedTab){
-
-    apiErrorContainer.classList.remove("active");
-  
-    if (clickedTab !== currentTab) {
-      currentTab.classList.remove("current-tab");
-      currentTab = clickedTab;
-      currentTab.classList.add("current-tab");
-      }
-  
-    if (!searchForm.classList.contains("active")) {
-        userInfoContainer.classList.remove("active");
-        grantAccessContainer.classList.remove("active");
-        searchForm.classList.add("active");
-      } 
-    else {
-        searchForm.classList.remove("active");
-        userInfoContainer.classList.remove("active");
-        getFromSessionStorage();
-      }
-  
-    console.log("Current Tab", currentTab);
-}
-  
-
-//fetch user Coordinates for default data
-function getLocation(){
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(showPosition);
-    }
-    else{
-        console.log("No geoLocation Support");
-    }
-}
-
-function showPosition(position){
-    let lat = position.coords.latitude;
-    let longi = position.coords.longitude;
-
-    console.log(lat);
-    console.log(longi);
-}
+let API_KEY = d91536744962e37b60058c47bd3e3eab;
