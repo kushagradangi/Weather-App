a00ec42bee0e808f9e1de157556cb26e490580d4
kushnotfromagra <kushagradangi10@gmail.com>
2025-01-24 23:53:16 +0530

    switchTab function

--- index.js
+++ index.js
@@ -7,3 +7,33 @@ const loadingScreen = document.querySelector(".loading-container");
 const userInfoContainer = document.querySelector(".user-info-container");
 
 let API_KEY = d91536744962e37b60058c47bd3e3eab;
+let currentTab = userTab;
+currentTab.classList.add("current-tab");
+getfromSessionStorage();
+
+function switchTab(clickedTab){
+    if(clickedTab != currentTab){
+        currentTab.classList.remove("current-tab");
+        currentTab = clickedTab;
+        currentTab.classList("current-tab");
+
+        if(!searchForm.classList.contains("active")){
+            userInfoContainer.classList.remove("active");
+            grantAccessContainer.classList.remove("active");
+            searchForm.classList.add("active");
+        }
+        else{
+            searchForm.classList.remove("active");
+            userInfoContainer.classList.remove("active");
+            getfromSessionStorage();
+        }
+    }
+}
+
+userTab.addEventListener("clicked",()=>{
+    switchTab(userTab);
+});
+
+searchTab.addEventListener("click", ()=>{
+    switchTab(searchTab);
+});
